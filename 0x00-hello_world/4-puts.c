884364cfd23258b8ff8f5bada2f19caef3eecdd8
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-16 19:11:54 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-int main ()
+int main(void)
 {
-	void puts()
-	{
-		printf("\"Programming is like building a multilingual puzzle\n");
-	}
-	puts();
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
