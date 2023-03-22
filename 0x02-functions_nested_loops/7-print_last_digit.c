6f66c7ed0784b034f5b00352ecb04b0213e635d1
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-21 19:33:49 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,12 @@
 int print_last_digit(int a)
 {
 	if (a > 0)
+	{
+		_putchar('0' + (a % 10));
 		return (a % 10);
+	}
 	else
-		return (-1 * a % 10);
+	{
+		_putchar('0' + (-1 * (a % 10));
+		return (-1 * (a % 10));
 }
