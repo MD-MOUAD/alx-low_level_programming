c15a3b45d2327fc33da27e33d8d5e337251d8d9c
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-21 19:26:59 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  print_last_digit - last digit
+ *
+ *@n: The int to print
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	if (a > 0)
+		return (a % 10);
+	else
+		return (-1 * a % 10);
+}
