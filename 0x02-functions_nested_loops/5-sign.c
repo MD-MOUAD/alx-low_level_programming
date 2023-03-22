b582ead9d84df0a46f67751c668070c866f900f2
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-21 18:53:46 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,29 @@
 #include "main.h"
 /**
- * print_sign - checks for alphabetic character
+ * print_sign - prints the sign of a number
  *
  * @n: the number to be checked
  *
- * Return: 1 if f n is greater than zero, 0 otherwise
+ * Return: 1 if n is greater than zero, 0 if n is zero,
+ * -1 if n is less than zero
  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else
+	{
+		if (n == 0)
+		{
+			return (0);
+			_putchar('0');
+		}
+		else
+		{
+			return (-1);
+			_putchar('-');
+		}
+	}
