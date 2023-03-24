258c713e048698bd0e14bfb828cd613e553f3ca2
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-23 19:38:41 -0700

    C_more_tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,10 +12,12 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (j = 0; j < n - 1; j++)
-		_putchar(' ');
 		for (i = 0; i < n; i++)
-		_putchar('\\');
+		{
+			for (j = 0; j < n - 1; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
