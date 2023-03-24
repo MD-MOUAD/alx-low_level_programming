29b517d2965cefd94efe9bfaa9e6b1b84bd90fd7
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-23 17:54:24 -0700

    C_more_tasks

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,3 +4,19 @@
  * more_numbers - a function that prints the numbers, from 0 to 9
  * Return: void
  */
+
+void more_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9' ; c++)
+		_putchar(c);
+	for (c = '0'; c <= '5' ; c++)
+	{
+		if (c == '5')
+			c = '\n';
+		else
+			_putchar('1');
+		_putchar(c);
+	}
+}
