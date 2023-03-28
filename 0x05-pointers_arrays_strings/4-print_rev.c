829a7e70f6f280417ae468ebc3b3860c2427f889
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-27 19:33:36 -0700

    pointers_array_strings_tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_rev - a function that prints a string in reverse
+  * followed by new line
+  * @s: a pointer to a string
+  * Return: (void)
+  */
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s += len - 1;
+	for (i = 0; i <= len; i++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
