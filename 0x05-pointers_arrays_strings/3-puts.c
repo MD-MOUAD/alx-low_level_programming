992c1456cf72426e7c5010e4b99f13910859d2bd
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-27 19:03:53 -0700

    pointers_array_strings_tasks

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _puts - a function that prints a string followed by new line
+  * to stdout
+  * @str: a pointer to a string
+  * Return: (void)
+  */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
