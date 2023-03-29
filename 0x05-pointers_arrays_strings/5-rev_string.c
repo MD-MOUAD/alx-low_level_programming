baaac0d7f0b0d4091460bb58e2dcadd5323a0d8c
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-28 18:28:37 -0700

    arrays_and_pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,17 @@
 
 void rev_string(char *s)
 {
-	int i, len = 0;
-	char *p;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	p = s;
-	while (*p != '\0')
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-		len++;
-		p++;
-	}
-	p--;
-	for (i = 0; i < len; i++)
-	{
-		s[i] = *p;
-		p--;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
