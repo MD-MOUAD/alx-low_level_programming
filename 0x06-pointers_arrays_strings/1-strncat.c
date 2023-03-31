9616e25fde6c23b5acc9fe88ecad16afd4ea04b8
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-03-31 10:12:41 -0700

    C_more_pointer_arrays_and_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "mainh.h"
+
+/**
+  * _strncat - a function that concatenates two strings
+  * @src: a pointer to source string
+  * @dest: a pointer to dest string
+  * @n: number of bytes to take from src
+  * Return: n bytes from the src string appending to the dest string
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int i, counter = 1;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		if (counter <= n)
+		{
+			*p = *src;
+			p++;
+			src++;
+		}
+	}
+	return (dest);
+}
