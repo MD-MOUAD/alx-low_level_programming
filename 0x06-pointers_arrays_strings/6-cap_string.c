8b726e70ee50bb347482ef5bb26ccac5967f6881
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-02 14:35:10 -0700

    C_pointers_more_tasks

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * cap_string - a function that capitalizes all words of a string
+  * @str: a pointer to the string
+  * Return: the string with all words capitalized
+  */
+char *cap_string(char *str)
+{
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		if (*p == '{' || *p == " " || *p == \t || *p == \n
+			|| *p == ',' || *p == ';' || *p == '.'
+			|| *p == '!' || *p == '?' || *p == '"'
+			|| *p == '(' || *p == ')' || *p == '}')
+			p++;
+		else if (*p >= 97 && *p <= 122)
+		{
+			*p -= 32;
+			do {
+				p++;
+			}
+				while (*p >= 97 && *p <= 122 || *p >= 65 && *p <= 90);
+		}
+		else
+			p++;
+	}
+}
