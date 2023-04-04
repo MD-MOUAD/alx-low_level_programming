7d401e6d37e84320b55ce9b502b6918c124f9599
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-03 17:58:25 -0700

    C-even-more-pointers-tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int check = 0;
 
-	while (*(s+i) != '\0')
+	while (*s != '\0')
 	{
-		if (*(s+i) == c)
-			return (s+i);
-		i++;
+		if (*s == c)
+			return (s);
+		s++;
 	}
 	return (NULL);
 }
