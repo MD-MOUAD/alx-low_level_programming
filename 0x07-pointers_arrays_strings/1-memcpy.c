c9d6425024e09522df0d3cf6348860fde9fee80a
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-03 17:29:06 -0700

    C-even-more-pointers-tasks

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: a pointer to to memory area dest
+ * @src: a pointer to to memory area src
+ * @n:number of bytes to copy
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ps = src, *pd = dest;
+
+	while (*ps != '\0' && n > 0)
+	{
+		*pd = *ps;
+		pd++;
+		ps++;
+		n--;
+	}
+	*pd = '\0';
+	return (dest);
+}
