8b116bcb984e89ec9bee438c325638264717b2e6
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-03 17:06:15 -0700

    C-even-more-pointers-tasks

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
   * _memset - a function that fills memory with a constant byte
 * @s: a pointer to the memory area
@@ -8,7 +9,7 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
