2a75d9b176e9be63b30e30bb252fe35eb2c5ab0c
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-05 15:21:25 -0700

    C-recursion-tasks

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,9 +9,10 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s == '\0')
-		return (len);
-	len++;
-	len = len + _strlen_recursion(s + 1);
+	if (*s != '\0')
+	{
+		len++;
+		len = len + _strlen_recursion(s + 1);
+	}
 	return (len);
 }
