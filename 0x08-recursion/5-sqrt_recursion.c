394e01e672f901acb764161bbca113723cc6e0e5
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-05 18:22:04 -0700

    C-recursion-tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,18 +9,18 @@
 int _sqrt_recursion(int n)
 {
     if (n < 0) {
-        return -1; // negative number has no natural square root
+        return -1;
     } else if (n == 0) {
-        return 0; // base case: square root of 0 is 0
+        return 0;
     } else {
         int root;
-		root = _sqrt_recursion(n - 1); // recursively calculate the square root of n-1
+		root = _sqrt_recursion(n - 1);
         if (root < 0) {
-            return -1; // n-1 does not have a natural square root, so neither does n
+            return -1;
         } else if (root * root == n) {
-            return root; // found the square root of n
+            return root;
         } else {
-            return root + 1; // try the next number
+            return root + 1;
         }
         
     }
