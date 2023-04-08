29e918476b04ffbfc2e5661400a735f995040922
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-08 13:21:48 -0700

    0x06-pointers_arrays_strings-tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,44 @@
+include "main.h"
+/**
+ * 10_pow - a function that calculate 10 to the power x
+ * @x: the power of 10
+ * Return: 10 to the power x
+ */
+int 10_pow(int x)
+{
+	int i, p = 1;
+
+	for (i = 1; i <= n; i++)
+	{
+		p = p * 10;
+	}
+	return (p);
+}
+/**
+ * print_number - a function that prints an integer
+ * @n: the number to print
+ * Return: print the number
+ */
+void print_number(int n)
+{
+	int i, ld, len = 0, a;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * (-1);
+	}
+	/* calculate the length of n*/
+	a = n;
+	while (a != 0)
+	{
+		len++;
+		a = a / 10;
+	}
+	for (i = len ; i < 0; i++)
+	{
+		a = n / 10_pow(i - 1);
+		ld = a % 10;
+		_putchar('0' + ld);
+	}
+}
