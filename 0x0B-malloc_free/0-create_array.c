f35c7688ac246c9f373fd5d4a5cd107f517c9b66
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-12 16:13:15 -0700

    malloc_free_tasks

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	char *arr = malloc(sizeof(char) * size);
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
