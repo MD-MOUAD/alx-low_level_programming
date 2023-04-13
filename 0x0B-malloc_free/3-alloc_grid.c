3c1e20333ab93b941e71a20687ae3642c0a5a3ed
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-12 19:28:49 -0700

    malloc_free_tasks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,13 +15,8 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc(sizeof(int*) * height);
-	if (grid == NULL)
-		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		grid = malloc(sizeof(int) * width);
-		if (grid == NULL)
-			return (NULL);
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
