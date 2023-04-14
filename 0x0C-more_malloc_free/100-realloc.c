5f7e8759da1e16da74ce1506fa214d25f6bdfc55
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-13 19:57:43 -0700

    C_more_malloc_free_tasks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,52 +1,51 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _realloc - a function that reallocates a memory block using malloc and free
- * The contents will be copied to the newly allocated space, in the range
- * from the start of ptr up to the minimum of the old and new sizes.
- * @ptr: a pointer to the memory previously allocated
- * @old_size: the size, in bytes, of the allocated space for ptr
- * @new_size: the new size, in bytes of the new memory block
- * Return: void
- */
+* *_realloc - reallocates a memory block using malloc and free
+* @ptr: pointer to the memory previsouly allocated by malloc
+* @old_size: size of the allocated memory for ptr
+* * @new_size: new size of the new memory block
+*
+* Return: pointer to the newly allocated memory block
+*/
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
 {
-	char *prealloc;
-	unsigned int i;
+char *ptr1;
+char *old_ptr;
+unsigned int i;
 
+if (new_size == old_size)
 
-	if (new_size == 0 && ptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (!ptr)
-	{
-		prealloc = malloc(new_size);
-		if (!prealloc)
-			return (NULL);
-	}
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size > old_size)
-	{
-		prealloc = malloc(new_size);
-		if (prealloc == NULL)
-			return (NULL);
-		for (i = 0; i < old_size; i++)
-			prealloc[i] = ptr[i];
-		free(ptr);
-		return (prealloc);
-	}
-	if (new_size < old_size)
-	{
-		prealloc = malloc(new_size);
-		if (prealloc == NULL)
-			return (NULL);
-		for (i = 0; i < new_size; i++)
-			prealloc[i] = ptr[i];
-		free(ptr);
-		return (prealloc);
-	}
+return (ptr);
+if (new_size == 0 && ptr)
+
+{
+free(ptr);
+return (NULL);
+}
+if (!ptr)
+
+return (malloc(new_size));
+ptr1 = malloc(new_size);
+if (!ptr1)
+
+return (NULL);
+old_ptr = ptr;
+if (new_size < old_size)
+
+{
+for (i = 0; i < new_size; i++)
+ptr1[i] = old_ptr[i];
+}
+if (new_size > old_size)
+
+{
+for (i = 0; i < old_size; i++)
+ptr1[i] = old_ptr[i];
+}
+free(ptr);
+return (ptr1);
 }
