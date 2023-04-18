412598d0d83c5e493d5b3c0a48d499582db1a8fa
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-17 19:09:52 -0700

    0x0D-preprocessor

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,7 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d  = malloc(sizeof(struct dog));
 	(*d).name = name;
 	d->age = age;
-	dog.owner = owner;
+	d->owner = owner;
 }
 
 
