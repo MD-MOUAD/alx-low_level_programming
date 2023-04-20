cd04182fc1d1680c43e1b45684b019688c6de698
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-19 19:39:41 -0700

    function-pointers-tasks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action != NULL || array != NULL)
-	{
-		for (i = 0; i < size; i++)
-			action(array[i]);
-	}
-	else 
+	if (action == NULL || array == NULL)
 		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
