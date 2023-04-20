f8b666aecb000b0cf40a9442144a66076ca0793b
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-19 19:31:06 -0700

    function-pointers-tasks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,8 +4,8 @@
  * array_iterator - a function that executes a function given as a parameter
  * on each element of an array.
  * @array: the array to exectute his elements
- * @size: the size of the array
- * @action: a pointer to the function 
+ * @size: the size of the arrau
+ * @action: a pointer to the function
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
