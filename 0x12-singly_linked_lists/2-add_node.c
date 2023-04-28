a2578bec01bc0e9cc1be51835fecc6e60fe24635
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-27 19:42:40 -0700

    Singly-linked-lists-tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,12 +16,14 @@ list_t *add_node(list_t **head, const char *str)
 
 	s = strdup(str);
 	l = malloc(sizeof(list_t));
-	if (l == NULL || str == NULL )
+	if (l == NULL || str == NULL || head == NULL)
 		return (NULL);
 
 	else
 	{
+
 		l->str = s;
+		l->len = strlen(s);
 		l->next = NULL;
 	}
 	l->next = *head;
