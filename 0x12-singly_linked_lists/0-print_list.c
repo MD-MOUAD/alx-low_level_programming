d2a3a8cb95d4ea6e8e7faaf6ce5e5f3ad1424114
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-27 18:41:07 -0700

    Singly-linked-lists-tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,6 +9,7 @@ size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 	char *next;
+	unsigned int len;
 
 	if (h == 0)
 		return (0);
@@ -16,9 +17,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		next = h->str;
+		len = h->len;
 		if (next == NULL)
-			next = "[0] (nil)";
-		printf("[%u] %s\n", h->len, next);
+		{
+			next = "(nil)";
+			len = 0;
+		}	
+		printf("[%u] %s\n", len, next);
 		h = h->next;
 		node++;
 	}
