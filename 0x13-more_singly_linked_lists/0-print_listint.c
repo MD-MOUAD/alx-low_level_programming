5f458232fd016a4103545d367dd9f9af0bf6dd12
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-28 21:27:38 -0700

    more_singly_linked_lists_tasks

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_listint - a function that prints all the elements of
+ * a listint_t list
+ * @h: a listint_t list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	if (h == NULL)
+		printf("NULL");
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
