5e314e075ce5298ac6239a7f2f98fb01d15703e9
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-04-30 11:54:01 -0700

    more_singly_linked_lists_tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,6 +12,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *l, *p;
 	int i;
 
+	if (*head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
