a1373aa16446f39417513fa5ccb2bea5c93e8220
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-01 04:02:10 -0700

    more_singly_linked_lists_tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,33 +10,36 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *h;
 	int i;
+	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new ==  NULL)
 		return (NULL);
 
 	new->n = n;
-	new->next = NULL;
 
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
+
 		return (new);
 	}
-	h = *head;
-	for (i = 0; i < idx; i++)
+
+	for (i = 0; *head != NULL; i++)
 	{
-		if ((i + 1) == idx)
+		if (i + 1 == (int) idx)
 		{
 			new->next = (*head)->next;
+
 			(*head)->next = new;
+
 			return (new);
 		}
-		(*head) = (*head)->next;
+
+		head = &(*head)->next;
 	}
-	*head = h;
+
 	return (NULL);
 }
