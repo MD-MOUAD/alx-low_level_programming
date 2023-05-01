5d6910060b0712af90927d5d309cce97bdedadca
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-01 05:05:03 -0700

    more_singly_linked_lists_tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = list->next;
 			list->next = new;
-			free(list);
 			return (new);
 		}
 		list = list->next;
