b59a75dc5c9778032927d8d89fb892d18a371fc4
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-02 15:34:29 -0700

    more_singly_linked_lists_tasks

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,15 +7,15 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev, *nex;
+	listint_t *prev, *next;
 
 	prev = NULL;
 	while (*head != NULL)
 	{
 		next = (*head)->next;
 		(*head)->next = prev;
-		prev = head;
-		head = next;
+		prev = *head;
+		*head = next;
 	}
-	return(prev);
+	return (prev);
 }
