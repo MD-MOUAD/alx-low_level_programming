f193687b606cb0b624ec3516d333394df3fae636
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-02 18:24:39 -0700

    more_singly_linked_lists_tasks

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -19,7 +19,7 @@ size_t print_listint_safe(const listint_t *head)
 		head = head->next;
 		if (current <= head)
 		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n 98", (void *)head, head->n);
 			n++;
 			break;
 		}
