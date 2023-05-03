d522ccdae4e752aff4eb3fab044c17c14f0440b8
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-02 18:16:37 -0700

    more_singly_linked_lists_tasks

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_listint_safe - a function that prints a listint_t linked list
+ * This function can print lists with a loop.
+ * @head: a pointer to listint_t list.
+ * Return: the number of nodes in the list
+ * If the function fails, exit the program with status 98.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current;
+	size_t n = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", head, head->n);
+		n++;
+		current = head;
+		head = head->next;
+		if (current <= head)
+		{
+			printf("-> [%p] %d\n", head, head->n);
+			exit(98);
+		}
+	}
+
+	return (n);
+}
