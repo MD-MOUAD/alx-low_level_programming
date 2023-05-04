cc6591aab9582ad590b9ebaebf4d6838b0fbbe77
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-04 14:41:54 -0700

    Bit_manipulation_tasks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,14 +9,14 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int r;
 
-	if(n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	if(n > 1)
+	if (n > 1)
 		print_binary(n >> 1);
 
 	r = n & 1;
-	_putchar('0'+ r);
+	_putchar('0' + r);
 }
