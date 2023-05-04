def278d290e4025eaa690222e81970e211f17db9
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-04 16:49:39 -0700

    Bit_manipulation_tasks

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,12 +11,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i, flip = 0;
 
-	for (i =0; i < 64; i++)
+	for (i = 0; i < 64; i++)
 	{
-		if ((n & 1) ^ (m & 1) == 1)
+		if (((n >> i) & 1) ^ ((m >> i) & 1) == 1)
 			flip++;
-		n = n >> i;
-		m = m >> i;
 	}
 	return (flip);
 }
