792c9d858eab390cd3a98ec3a40d4e401948a848
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-08 17:29:27 -0700

    FILE-I/O_tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,20 +8,24 @@
  **/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int fd, wr, i = 0;
 
-	fd = open(filename, O_APPEND);
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
-	wr = write(fd, text_content, i);
-	if (wr == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[i])
+			i++;
+		wr = write(fd, text_content, i);
+		if (wr == -1)
+			return (-1);
+	}
 
-
-
+	close(fd);
 	return (1);
-
 }
