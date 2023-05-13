795eaed618eb92f48a860ea791d42d5301986f4d
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-05-13 05:10:23 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,19 +1,21 @@
-#include "main.h"
-
-void print_error_1(char *msg, char *opt, int status, int fd1);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+void close_fd(int fd);
 void print_error(char *msg, char *opt, int status);
-
 /**
- * main - Copies the content of a file-from to file_to file
+ * main - check code
  * @argc: Numbers of arguments
  * @argv: Arguments
  *
- * Return: Return 0
+ * Return: Always 0
  **/
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, b_readed, b_writed;
-	char buff[1024];
+	int fd_to, fd_from, r_bytes, w_bytes;
+	char buffer[1024];
 
 	if (argc != 3)
 		print_error("Usage: cp file_from file_to", "", 97);
@@ -22,47 +24,48 @@ int main(int argc, char **argv)
 	if (fd_from == -1)
 		print_error("Error: Can't read from file ", argv[1], 98);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_to == -1)
-		print_error_1("Error: Can't write to ", argv[2], 99, fd_from);
-
-	b_readed = read(fd_from, buff, 1024);
-	while (b_readed > 0)
 	{
-		b_writed = write(fd_to, buff, b_readed);
-		if (b_readed != b_writed)
-			print_error_1("Error: Can't write to ", argv[2], 99, fd_from);
-
-		b_readed = read(fd_from, buff, 1024);
+		close_fd(fd_from);
+		print_error("Error: Can't write to ", argv[2], 99);
 	}
 
-	if (b_readed == -1)
+	r_bytes = read(fd_from, buffer, 1024);
+	while (r_bytes > 0)
+	{
+		w_bytes = write(fd_to, buffer, r_bytes);
+		if (w_bytes != r_bytes)
+		{
+			close_fd(fd_from);
+			print_error("Error: Can't write to ", argv[2], 99);
+		}
+		r_bytes = read(fd_from, buffer, 1024);
+	}
+	if (r_bytes == -1)
 		print_error("Error: Can't read from file ", argv[1], 98);
-
-	b_readed = close(fd_from);
-	if (b_readed < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-
-	b_writed = close(fd_to);
-	if (b_writed < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
-	return (0);
+	close_fd(fd_from);
+	close_fd(fd_to);
 }
-
 /**
- * print_error_1 - Print the error and close the file descriptor
- * @msg: Error message to show
- * @opt: Optional arguments
- * @status: Exit status code
- * @fd1: File descriptor
- **/
-void print_error_1(char *msg, char *opt, int status, int fd1)
+ * close_fd - close file descriptor
+ * @fd: file descriptor
+ * Return: void
+ */
+void close_fd(int fd)
 {
-	if (fd1 > 0)
-		close(fd1);
+	int err;
 
-	print_error(msg, opt, status);
+	if (fd > 0)
+	{
+		err = close(fd);
+		if (err == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(100);
+		}
+	}
 }
 
 /**
