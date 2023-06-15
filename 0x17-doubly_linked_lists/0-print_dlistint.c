a4eaace41dd7f3d2928f452ec221b76c815fbc17
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-06-15 08:32:55 -0700

    first commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+	const dlistint_t *head = h;
+
+	while(head)
+	{
+		nodes++;
+		printf("%d\n", head->n);
+		head = head->next;
+	}
+	return (nodes);
+}
