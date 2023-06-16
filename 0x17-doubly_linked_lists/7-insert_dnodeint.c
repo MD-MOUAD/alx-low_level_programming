29f22b4a420de3a694235bf1839d22def99e84ba
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-06-15 17:56:21 -0700

    dobly linked lists tasks

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: the head of the list
+ * @idx: the position to insert the new node
+ * @n: data of the new node
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *next_node, *l = *head;
+	int index = 0;
+
+	if (idx == 0)
+		add_dnodeint(&(*head), n);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	while (l)
+	{
+		next_node = l->next;
+		index++;
+		if (idx == index)
+		{
+			l->next = new;
+			new->prev = l;
+			if (next_node)
+				next_node->prev = new;
+			return (new);
+		}
+		l = l->next;
+	}
+	free(new);
+	return (NULL);
+
+}
