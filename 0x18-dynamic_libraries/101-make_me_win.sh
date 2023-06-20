88d4694fdbd3f32a86dbd5074915de2adf80f1c2
MOUAD KHANOUCH <mouadmouadkhanouch.com>
2023-06-19 18:39:19 -0700

    advanced

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#!/bin/bash
-wget -O /tmp/win.so https://github.com/Ze-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+wget -O /tmp/win.so https://github.com/MD-MOUAD/alx-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
