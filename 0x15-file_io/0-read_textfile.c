04ea0e9a73ef5e4cf6aa24365f0ed8b1b87612f5
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-08-09 15:37:37 +0100

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,36 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, n, r_bytes;
+	int fd, r_bytes, n;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-
-	buffer = malloc((sizeof(char) * letters) + 1);
-	if (buffer == NULL)
+	buffer = malloc(sizeof(char) * letters + 1);
+	if (!buffer)
 		return (0);
-
 	r_bytes = read(fd, buffer, letters);
-	buffer[r_bytes] = '\0';
-
 	close(fd);
-
-	if (r_bytes == -1)
+	if (r_bytes < 0)
 	{
 		free(buffer);
 		return (0);
 	}
-
-	n =	write(STDOUT_FILENO, buffer, r_bytes);
-
+	buffer[r_bytes] = '\0';
+	n = write(STDOUT_FILENO, buffer, r_bytes);
 	free(buffer);
-	if (n == -1)
+	if (n < 0)
 		return (0);
-
 	return (n);
 }
