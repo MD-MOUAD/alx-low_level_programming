30ab9bb390c23d107f91737880ff57c153777154
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-09-14 02:10:21 +0100

    fix betty

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -16,9 +16,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -31,8 +31,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	 unsigned long int size;
+	 hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -80,10 +80,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-	 unsigned long int size;
-	 shash_node_t **array;
-	 shash_node_t *shead;
-	 shash_node_t *stail;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
